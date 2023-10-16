fbe0ded0b909578e5aeec3fb39f3b502b29c487f
Benjamin Tucker <benjamin.tucker@hey.com>
2023-10-16 14:56:02 -0700

    sln dir

---
+++ GrabberTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using NodeGrabber;
+
+namespace GrabberTest
+{
+    internal partial class Program
+    {
+        [Grab]
+        int baoana;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Woorld!");
+        }
+
+        void Foo()
+        {
+            GrabNodes();
+        }
+    }
+}
