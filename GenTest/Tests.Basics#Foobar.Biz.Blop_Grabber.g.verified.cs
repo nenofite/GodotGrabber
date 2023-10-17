fed8312edc09ba604e591e9f2034192115d8e61c
Benjamin Tucker <benjamin.tucker@hey.com>
2023-10-16 17:01:02 -0700

    cleanup snaps

---
+++ GenTest/Tests.Basics#Foobar.Biz.Blop_Grabber.g.verified.cs
@@ -0,0 +1,13 @@
+﻿//HintName: Foobar.Biz.Blop_Grabber.g.cs
+
+namespace Foobar.Biz
+{
+    partial class Blop
+    {
+        void GrabNodes()
+        {
+// ShouldInclude = GetNode<int>("%ShouldInclude");
+// ShouldIncludeQualType = GetNode<System.Text.StringBuilder>("%ShouldIncludeQualType");
+        }
+    }
+}
