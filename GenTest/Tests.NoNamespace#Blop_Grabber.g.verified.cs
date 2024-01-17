dfa411613349910c5f3b5615631becc7a3168669
Benjamin Tucker <benjamin.tucker@hey.com>
2024-01-17 13:30:17 -0800

    NodeGrabber => GodotGrabber

--- GenTest/Tests.NoNamespace#Blop_Grabber.g.verified.cs
+++ GenTest/Tests.NoNamespace#Blop_Grabber.g.verified.cs
@@ -2,10 +2,15 @@
 
 partial class Blop
 {
+    /// <summary>
+    /// Set the values of all fields marked with <c>[Grab]</c>. You should usually call this in <c>_Ready()</c>.
+    /// 
+    /// The current value of each field will be overwritten.
+    /// </summary>
     void GrabNodes()
     {
-ShouldInclude = GetNode<int>("%ShouldInclude");
-ShouldIncludeQualType = GetNode<System.Text.StringBuilder>("%ShouldIncludeQualType");
-withPath = GetNode<bool>("special path");
+        ShouldInclude = GetNode<int>("%ShouldInclude");
+        ShouldIncludeQualType = GetNode<System.Text.StringBuilder>("%ShouldIncludeQualType");
+        withPath = GetNode<bool>("special path");
     }
 }
